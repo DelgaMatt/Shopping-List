1c187dacd734aa9b878a1b13bfaaeabfb50439de
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-11-15 13:54:16 -0800

    added get request to grocery list page for displaying items in firebase

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:shopping/data/categories.dart';
 import 'package:shopping/models/category.dart';
-import 'package:shopping/models/grocery_item_model.dart';
+// import 'package:shopping/models/grocery_item_model.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -23,15 +23,16 @@ class _NewItemState extends State<NewItem> {
   var _enteredQuantity = 1;
   var _selectedCategory = categories[Categories.vegetables]!;
 
-  void _saveItem() {
+  void _saveItem() async {
     if (_formKey.currentState!.validate()) {
       _formKey.currentState!.save();
-    } //will trigger the 'onSaved' function
+      //will trigger the 'onSaved' function
 
-    final url = Uri.https(
-        'flutter-shopping-list-4d113-default-rtdb.firebaseio.com',
-        'shopping-list.json'); //creating a url
-    http.post(url,
+      final url = Uri.https(
+          'flutter-shopping-list-4d113-default-rtdb.firebaseio.com',
+          'shopping-list.json'); //creating a url
+      final response = await http.post(
+        url,
         headers: {
           'Content-Type': 'application/json',
         },
@@ -41,13 +42,18 @@ class _NewItemState extends State<NewItem> {
             'quantity': _enteredQuantity,
             'category': _selectedCategory.title,
           },
-        ));
-    // Navigator.of(context).pop(GroceryItem(
-    //     id: DateTime.now().toString(),
-    //     name: _enteredName,
-    //     quantity: _enteredQuantity,
-    //     category:
-    //         _selectedCategory)); //how we're passing new grocery item to the grocery list page. popping the screen off and sending information to parent page
+        ),
+      );
+
+      print(response.body);
+      print(response.statusCode);
+
+      if (!context.mounted) {
+        return;
+      }
+
+      Navigator.of(context).pop(); //popping screen once the request is recieved
+    }
   }
 
   @override
