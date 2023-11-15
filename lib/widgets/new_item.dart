c4207bb0925229d4d83a8fb99f427846a2ec9c1d
DelgaMatt <115049801+DelgaMatt@users.noreply.github.com>
2023-11-14 23:33:59 -0800

    global keys for form items.

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shopping/data/categories.dart';
+import 'package:shopping/models/category.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -11,6 +12,18 @@ class NewItem extends StatefulWidget {
 }
 
 class _NewItemState extends State<NewItem> {
+  final _formKey = GlobalKey<
+      FormState>(); //if the build widget is executed again, the form widget will not be rebuilt and will keep it's internal state. which is what will communicate to flutter if it needs to display some validator messaging or not.
+  var _enteredName = '';
+  var _enteredQuantity = 1;
+  var _selectedCategory = categories[Categories.vegetables]!;
+
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+    } //will trigger the 'onSaved' function
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,6 +31,7 @@ class _NewItemState extends State<NewItem> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
+          key: _formKey,
           child: Column(
             children: [
               TextFormField(
@@ -26,6 +40,7 @@ class _NewItemState extends State<NewItem> {
                 decoration: const InputDecoration(
                   label: Text('Name'),
                 ),
+                initialValue: _enteredName,
                 validator: (value) {
                   //another property given to us by using the form widget
                   if (value == null ||
@@ -36,6 +51,9 @@ class _NewItemState extends State<NewItem> {
                   }
                   return null;
                 },
+                onSaved: (value) {
+                  _enteredName = value!;
+                },
               ),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
@@ -44,17 +62,22 @@ class _NewItemState extends State<NewItem> {
                     child: TextFormField(
                       decoration:
                           const InputDecoration(label: Text('Quantity')),
-                      initialValue: '1',
+                      keyboardType: TextInputType.number,
+                      initialValue: _enteredQuantity.toString(),
                       validator: (value) {
-                  //another property given to us by using the form widget
-                  if (value == null ||
-                      value.isEmpty ||
-                      int.tryParse(value) == null ||//then we know its not a valid number
-                      int.tryParse(value)! <= 0) { 
-                    return 'Must be a valid positive number';
-                  }
-                  return null;
-                },
+                        //another property given to us by using the form widget
+                        if (value == null ||
+                            value.isEmpty ||
+                            int.tryParse(value) ==
+                                null || //then we know its not a valid number
+                            int.tryParse(value)! <= 0) {
+                          return 'Must be a valid positive number';
+                        }
+                        return null;
+                      },
+                      onSaved: (value) {
+                        _enteredQuantity = int.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(
@@ -62,25 +85,31 @@ class _NewItemState extends State<NewItem> {
                   ),
                   Expanded(
                     child: DropdownButtonFormField(
-                        //selected item will be stored in ui - feature of the form
-                        //form specific version thats supported by forms
-                        items: [
-                          for (final category in categories
-                              .entries) //dummy data is a map, so we add .entries which returns every item in list
-                            DropdownMenuItem(
-                              value: category.value,
-                              child: Row(children: [
-                                Container(
-                                    width: 16,
-                                    height: 16,
-                                    color: category.value.color),
-                                const SizedBox(width: 6),
-                                Text(category.value
-                                    .title) //here we are iteratiing through the list via the .entries method
-                              ]),
-                            )
-                        ],
-                        onChanged: (value) {}),
+                      //selected item will be stored in ui - feature of the form
+                      //form specific version thats supported by forms
+                      value: _selectedCategory,
+                      items: [
+                        for (final category in categories
+                            .entries) //dummy data is a map, so we add .entries which returns every item in list
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(children: [
+                              Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color),
+                              const SizedBox(width: 6),
+                              Text(category.value
+                                  .title) //here we are iteratiing through the list via the .entries method
+                            ]),
+                          )
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
@@ -90,12 +119,16 @@ class _NewItemState extends State<NewItem> {
               Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  TextButton(onPressed: () {}, child: const Text('Reset')),
+                  TextButton(
+                      onPressed: () {
+                        _formKey.currentState!.reset();
+                      },
+                      child: const Text('Reset')),
                   const SizedBox(
                     width: 8,
                   ),
                   ElevatedButton(
-                      onPressed: () {}, child: const Text('Add Item'))
+                      onPressed: _saveItem, child: const Text('Add Item'))
                 ],
               )
             ],
